aa3234d58c45897217b32f2a999e5b8af5c118f9
gortxut <isasigor10@gmail.com>
2024-01-18 18:49:07 +0100

    ssl certificate

--- src/run.py
+++ src/run.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from routes import routes
 import uuid
+import subprocess
 
 app = Flask(__name__)
 app.secret_key = uuid.uuid4().hex
 app.register_blueprint(routes)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, ssl_context=('/run/secrets/ssl_cert','/run/secrets/ssl_key'))
